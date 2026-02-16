4dbeb7fb729bcd09c9fdef38d3c300e2bef9c562
Goazo <kessejay1@gmail.com>
2026-02-16 13:28:55 -0600

    Update VideoAdsPatch.kt

--- app/src/main/java/io/github/chsbuffer/revancedxposed/youtube/ad/video/VideoAdsPatch.kt
+++ app/src/main/java/io/github/chsbuffer/revancedxposed/youtube/ad/video/VideoAdsPatch.kt
@@ -5,7 +5,7 @@ import io.github.chsbuffer.revancedxposed.patch
 
 val VideoAds = patch(
     name = "Video ads",
-    description = "Adds an option to remove ads in the video player.",
+    description = "Adds an option to remove ads in the video player when you watch videos.",
 ) {
     ::loadVideoAdsFingerprint.hookMethod(XC_MethodReplacement.DO_NOTHING)
 }
